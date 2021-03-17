d91437f27227dd40c3f765369c376144ca3875d9
risonakamo <hmykanae@gmail.com>
2021-03-16 21:03:23 -0400

    initial commit react web setup

---
+++ web/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.less";
+
+function RemoteRenamerIndex():JSX.Element
+{
+  return <>
+
+  </>;
+}
+
+function main()
+{
+  ReactDOM.render(<RemoteRenamerIndex/>,document.querySelector(".main"));
+}
+
+window.onload=main;
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const MiniCssExtractPlugin=require("mini-css-extract-plugin");
+const ForkTsCheckerWebpackPlugin=require("fork-ts-checker-webpack-plugin");
+// const CopyPlugin=require("copy-webpack-plugin");
+const WebpackBar=require("webpackbar");
+
+module.exports={
+    mode:"development",
+    entry:{
+        // add entry points here
+        index:"./web/index.tsx"
+    },
+    output:{
+        path:`${__dirname}/build`,
+        filename:"[name]-build.js"
+    },
+
+    module:{
+        rules:[
+            {
+                test:/\.(tsx|ts|js|jsx)$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:"babel-loader",
+                    options:{
+                        presets:["@babel/preset-react","@babel/preset-typescript"]
+                    }
+                }
+            },
+            {
+                test:/\.(less|css)$/,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    {loader:"css-loader",options:{url:false}},
+                    {loader:"less-loader"}
+                ]
+            }
+        ]
+    },
+
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename:"[name]-build.css"
+        }),
+
+        new ForkTsCheckerWebpackPlugin(),
+        new WebpackBar()
+
+        // new CopyPlugin([
+        //     {from:"src/index.html",to:"../"}
+        // ]),
+    ],
+
+    // optimization:{
+    //     splitChunks:{
+    //         chunks:"all",
+    //         automaticNameDelimiter:"-"
+    //     }
+    // },
+
+    resolve:{
+        extensions:[".tsx",".ts",".js",".jsx"],
+        // alias:{
+        //     top:`${__dirname}`
+        // }
+    },
+
+    stats:{
+        entrypoints:false,
+        modules:false,
+        chunks:false,
+        // assets:false
+    },
+
+    devtool:"eval-source-map"
+};
